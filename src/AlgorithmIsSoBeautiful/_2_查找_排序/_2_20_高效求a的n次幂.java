98932671495efcfe7dca65981fa98b5872a9b106
xzj <mrx.zimo@gmail.com>
2021-01-30 23:40:48 +0800

    文件结构修改

--- src/AlgorithmIsSoBeautiful/_2_查找_排序/_2_20_高效求a的n次幂.java
+++ src/AlgorithmIsSoBeautiful/_2_查找_排序/_2_20_高效求a的n次幂.java
@@ -12,24 +12,34 @@ public class _2_20_高效求a的n次幂 {
     public static void main(String[] args) {
         ComputingTime computingTime = new ComputingTime();
         _2_20_高效求a的n次幂 c = new _2_20_高效求a的n次幂();
+        int n = 15;
         computingTime.begin();
-        System.out.println(c.pow0(2,16));
+        System.out.println(c.pow0(2, n));
         computingTime.end();
         System.out.println();
         computingTime.begin();
-        System.out.println(c.pow(2,16));
+        System.out.println(c.pow(2, n));
         computingTime.end();
     }
 
-    public int pow0(int a,int n){
-        int result= 1;
+    public int pow0(int a, int n) {
+        int result = 1;
         for (int i = 0; i < n; i++) {
-            result*=a;
+            result *= a;
         }
         return result;
     }
 
-    public int pow(int a,int n){
-
+    public int pow(int a, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        int result = a;
+        int ex = 1;
+        while ((ex << 1) <= n) {
+            result = result * result;
+            ex <<= 1;
+        }
+        return result * pow(a, n - ex);
     }
 }
---
+++ src/Utils/myUtils.java
@@ -0,0 +1,50 @@
+package Utils;
+
+/**
+ * TODO
+ *
+ * @author zixi
+ * @version 1.0
+ * @date 21/01/30 下午 10:49
+ */
+public class myUtils {
+    /**
+     * 生成整型的随机数组
+     *
+     * @param len 数组长度
+     * @param min 最小值
+     * @param max 最大值
+     * @return 返回生成好的数组
+     */
+    public int[] intRandomArray(int len, int min, int max) {
+        int[] arr = new int[len];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * max + min);
+        }
+        return arr;
+    }
+
+    long beginTime;
+    long endTime;
+
+    /**
+     * 算法开始计时
+     *
+     * @return
+     */
+    public long beginCompute() {
+        beginTime = System.currentTimeMillis();
+        return beginTime;
+    }
+
+    /**
+     * 算法结束计时
+     *
+     * @return
+     */
+    public long endCompute() {
+        endTime = System.currentTimeMillis() - beginTime;
+        System.out.print(endTime + "ms");
+        return endTime;
+    }
+}
