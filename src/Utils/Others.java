3fa29593ef5fd3cc6f21105e2287efa877a57171
xzj <mrx.zimo@gmail.com>
2021-02-13 10:17:42 +0800

    新增 findDigitsOfNumber getDigitOn

--- src/Utils/Others.java
+++ src/Utils/Others.java
@@ -11,10 +11,42 @@ public class Others {
 
     /**
      * 判断一个数是不是奇数
+     *
      * @param number 被判断的数
      * @return 返回布尔值
      */
     public static boolean isOdd(int number) {
         return (number & 0x1) == 1;
+    }
+
+    /**
+     * findDigitsOfNumber
+     *
+     * @param number 被判断的数
+     * @return 返回最高位的位数
+     */
+    public static int findDigitsOfNumber(int number) {
+        int result = 0;
+        while (number / 10 != 0) {
+            result++;
+            number /= 10;
+        }
+        return result;
+    }
+
+    /**
+     * getDigitOn
+     *
+     * @param number 被找的数
+     * @param d      第几位
+     * @return 数的第d位的数
+     */
+    public static int getDigitOn(int number, int d) {
+        int result = 0;
+        for (int i = 0; i < d; i++) {
+            result = number % 10;
+            number /= 10;
+        }
+        return result;
     }
 }
