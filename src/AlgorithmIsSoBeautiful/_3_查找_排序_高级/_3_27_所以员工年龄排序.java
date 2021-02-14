f354f55bebb91807cba7e0a1f019e9265e4aeb62
xzj <mrx.zimo@gmail.com>
2021-02-14 23:36:37 +0800

    完成 _3_27_所以员工年龄排序

---
+++ src/AlgorithmIsSoBeautiful/_3_查找_排序_高级/_3_27_所以员工年龄排序.java
@@ -0,0 +1,17 @@
+package AlgorithmIsSoBeautiful._3_查找_排序_高级;
+
+/**
+ * _3_27_所以员工年龄排序
+ * 输入第一行为一个整数n(1 <= n <= 1000000)，代表公司内员工人数
+ * 输入第二行包括n 个整数，代表公司内每个员工的年龄，age(1<=age<=99)
+ * 很简单,用计数排序就好
+ *
+ * @author zixi
+ * @version 1.0
+ * @date 21/02/14 下午 9:18
+ */
+public class _3_27_所以员工年龄排序 {
+    public static void main(String[] args) {
+        System.out.println("计数排序即可");
+    }
+}
