37e78d120cd392d3fbf31a3065555def8bc8bdd0
xzj <mrx.zimo@gmail.com>
2021-02-16 12:33:31 +0800

    complete _4_2_零所在行列清零

---
+++ src/AlgorithmIsSoBeautiful/_4_矩阵/_4_2_零所在行列清零.java
@@ -0,0 +1,46 @@
+package AlgorithmIsSoBeautiful._4_矩阵;
+
+/**
+ * _4_2_零所在行列清零
+ *
+ * @author zixi
+ * @version 1.0
+ * @date 21/02/16 上午 11:52
+ */
+public class _4_2_零所在行列清零 {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {31, 32, 33, 34, 21},
+                {35, 36, 37, 38, 22},
+                {39, 0, 11, 12, 23},
+                {13, 14, 15, 0, 24},
+                {25, 26, 27, 28, 29}
+        };
+        Utils.AboutArray.printTwoDimensionalIntArray(matrix);
+        System.out.println("-----------------");
+        new _4_2_零所在行列清零().clearZeros(matrix);
+        Utils.AboutArray.printTwoDimensionalIntArray(matrix);
+    }
+
+    private void clearZeros(int[][] matrix) {
+        int M = matrix.length;
+        int N = matrix[0].length;
+        int[] rowRecord = new int[M];
+        int[] colRecord = new int[N];
+        for (int i = 0; i < rowRecord.length; i++) {
+            for (int j = 0; j < colRecord.length; j++) {
+                if (matrix[i][j] == 0) {
+                    rowRecord[i] = 1;
+                    colRecord[j] = 1;
+                }
+            }
+        }
+        for (int i = 0; i < rowRecord.length; i++) {
+            for (int j = 0; j < colRecord.length; j++) {
+                if (rowRecord[i] == 1 || colRecord[j]== 1) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
