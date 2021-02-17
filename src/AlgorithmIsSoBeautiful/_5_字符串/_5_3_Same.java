6c85c29925536a8a60c58885c8e8dab2b2ec86ef
xzj <mrx.zimo@gmail.com>
2021-02-17 22:23:33 +0800

    完成 _5_3_Same

---
+++ src/AlgorithmIsSoBeautiful/_5_字符串/_5_3_Same.java
@@ -0,0 +1,15 @@
+package AlgorithmIsSoBeautiful._5_字符串;
+
+/**
+ * _5_3_Same
+ * 变形词：两个串有相同的字符及数量组成
+ * 方法一(没写)：转成字符数组，去排序，然后对比是否相同。但是复杂度为O(NlgN)
+ * 方法二(前提,字符串由ASCII码组成)：用计数排序的思路
+ * TODO 代码没写加快进度fast#u
+ *
+ * @author zixi
+ * @version 1.0
+ * @date 21/02/17 下午 3:17
+ */
+public class _5_3_Same {
+}
