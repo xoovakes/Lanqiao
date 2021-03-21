b6cca7b35d98e02aa3655827b555d905e0b9b7ad
xzj <mrx.zimo@gmail.com>
2021-03-22 01:08:13 +0800

    更新

--- src/_刷题/Dotcpp/_1106_奖学金.java
+++ src/_刷题/Dotcpp/_1106_奖学金.java
@@ -1,9 +1,64 @@
 package _刷题.Dotcpp;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 //https://www.dotcpp.com/oj/problem1106.html
 
 public class _1106_奖学金 {
+	// 用面向对象简单很多
+	// TODO:21/03/22 漏了排语文的成绩了
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		Score[] score = new Score[N];
+		for (int i = 0; i < N; i++) {
+			score[i] = new Score(i + 1, sc.nextInt(), sc.nextInt(), sc.nextInt());
+		}
+		Arrays.sort(score);
+		// 结果
+		int i = 0;
+		while (i <= 5) {
+			if ((N < 5 && i == N - 1) || i == 4) {
+				System.out.print(score[i].getId() + " " + score[i].getTotal());
+				break;
+			}
+			System.out.println(score[i].getId() + " " + score[i].getTotal());
+			i++;
+		}
+	}
+
+	private static class Score implements Comparable<Score> {
+		int id;
+		int a;
+		int b;
+		int c;
+
+		public int getId() {
+			return id;
+		}
+
+		public Score(int id, int a, int b, int c) {
+			super();
+			this.id = id;
+			this.a = a;
+			this.b = b;
+			this.c = c;
+		}
+
+		public int getTotal() {
+			return a + b + c;
+		}
+
+		@Override
+		public int compareTo(Score o) {
+			int x = o.getTotal() - this.getTotal();
+			if (x == 0) {
+				return this.getId() - o.getId();
+			} else {
+				return x;
+			}
+		}
 
 	}
 }
